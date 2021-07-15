ce4456c623092c402b61a4e96ec2ff5aefc5f719
Greg Hughes <dcgg_101@hotmail.com>
2021-07-14 20:12:40 -0600

    added getLocation

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -2,6 +2,8 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:location/location.dart';
+import 'package:path_provider/path_provider.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -11,7 +13,12 @@ class LocationInput extends StatefulWidget {
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
 
-  Future<Void> _getCurrentUserLocation() async {}
+  Future<Void> _getCurrentUserLocation() async {
+    print("hit");
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +42,7 @@ class _LocationInputState extends State<LocationInput> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FlatButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentUserLocation,
               icon: Icon(Icons.location_on),
               label: Text('CurrentLocation'),
               textColor: Theme.of(context).primaryColor,
